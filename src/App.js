bd713cd7103ee4d8ad2151dc94e8641f99e46ce3
Vinicius Mussato <vinicius.mussato@gmail.com>
2022-09-28 16:34:12 -0300

    todo component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
+import Todo from "./components/Todo";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         setTodo={setTodo} 
         todoList={todoList} 
         setTodoList={setTodoList}/>
+        <Todo />
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import styles from '../style.module.css';
+
+const Todo = () => {
+    return <div>
+        <div className={styles.todoitem}>
+            <h3 className={styles.todoname}>Todo</h3>
+            <button className={styles.deletebutton}>Done</button>
+        </div>
+    </div>
+}
+
+export default Todo;
